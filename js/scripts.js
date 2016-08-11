51f99eb6974b113f3d90e3951245d19ba2c365fb
Ewa Manek and Michael Andrade <ewa@>
2016-08-11 11:59:19 -0700

    add content, branching logic and warnings

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var name = $("#nameInput").val();
+    var age = parseInt($("#ageInput").val());
+    var date = parseInt($("input:radio[name=idealDate]:checked").val());
+    if(!name || !age || !date){
+      if(!name){
+        $("#nameBox").addClass("has-error");
+      }
+      if(!age){
+        $("#ageBox").addClass("has-error");
+      }
+      if(!date) {
+        $(".radio").addClass("has-error");
+      }
+    } else {
+    if(age < 18){
+      alert("Uhhh. Try again when you're 18, kid.")
+    } else {
+      if(date === 1 && age > 40){
+        window.location.href = "gandalf.html";
+      } else if(date===1 && age <=40){
+        window.location.href = "harry.html";
+      } else if(date===2 && age > 40){
+        window.location.href = "sauron.html";
+      } else {
+        window.location.href = "draco.html";
+      }
+    }
+    }
+
+  });
+});
